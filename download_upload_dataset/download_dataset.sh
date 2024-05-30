06b43cb1d2ed7a13d4fa4d4f75d9420d06856595
Mihai M <mihaimituca@gmail.com>
2024-05-30 20:07:06 +0300

    Modified the bash script to upload the dataset to S3

--- download_upload_dataset/download_dataset.sh
+++ download_upload_dataset/download_dataset.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-kaggle datasets download -d rahulvyasm/netflix-movies-and-tv-shows
+AWS_PROFILE=repo1-admin
+DATASET=netflix-movies-and-tv-shows
+FILE_NAME=${DATASET}.zip
+BUCKET=mituca-repo1-raw-data
 
+kaggle datasets download -d rahulvyasm/${DATASET}
+
+aws s3 cp ${FILE_NAME} s3://${BUCKET}/raw_input/${FILE_NAME} --profile ${AWS_PROFILE}
+
+rm ${FILE_NAME}
