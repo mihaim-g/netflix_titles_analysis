094b11d265b9ea82ed19094ba9532862819e51cd
Mihai M <mihaimituca@gmail.com>
2024-06-01 15:26:35 +0300

    Added test for getting AWS credentials

---
+++ tests/test_read_dataset.py
@@ -0,0 +1,20 @@
+from src.pyspark.read_dataset import get_aws_credentials
+
+
+def test_get_aws_credentials(mocker):
+    mock_session = mocker.Mock()
+    mock_credentials = mocker.Mock()
+    mock_credentials.access_key = 'fake_access_key'
+    mock_credentials.secret_key = 'fake_secret_key'
+
+    mock_session.get_credentials.return_value = mock_credentials
+
+    mocker.patch('boto3.Session', return_value=mock_session)
+
+    profile = 'test_profile'
+    access_key, secret_key = get_aws_credentials(profile)
+
+    assert access_key == 'fake_access_key'
+    assert secret_key == 'fake_secret_key'
+    
+
