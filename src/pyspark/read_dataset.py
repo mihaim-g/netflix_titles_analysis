077e47a21db8ebe908de27df3ebe05978b5445ea
Mihai M <mihaimituca@gmail.com>
2024-06-01 15:11:37 +0300

    Added return type for function

--- src/pyspark/read_dataset.py
+++ src/pyspark/read_dataset.py
@@ -1,5 +1,6 @@
 import os
 import boto3
+import pyspark
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
 
@@ -13,7 +14,7 @@ def get_aws_credentials(profile: str) -> tuple:
     return(credentials.access_key, credentials.secret_key)
 
 
-def get_spark_session(aws_creds: tuple):
+def get_spark_session(aws_creds: tuple) -> pyspark.sql.session.SparkSession:
     conf = SparkConf()
     conf.set('fs.s3a.access.key', aws_creds[0])
     conf.set('fs.s3a.secret.key', aws_creds[1])
@@ -23,5 +24,6 @@ def get_spark_session(aws_creds: tuple):
 if __name__ == '__main__':
     creds = get_aws_credentials(aws_profile)
     spark = get_spark_session(creds)
+    print(type(spark))
     df = spark.read.csv(FILE_NAME, inferSchema=True)
     df.show(10)
