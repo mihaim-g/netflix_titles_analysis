5e4be82d343f925d1a32f763d670c2067bb13e90
Mihai M <mihaimituca@gmail.com>
2024-06-04 18:40:54 +0300

    Small changes

--- src/pyspark/read_dataset.py
+++ src/pyspark/read_dataset.py
@@ -2,6 +2,7 @@ import os
 from utils.aws_utils import AWSUtils
 from utils.spark_session import CreateSparkSession
 
+# TODO: move to a different module and class
 
 aws_profile=os.environ['AWS_PROFILE']
 FILE_NAME='s3a://mituca-repo1-raw-data/raw_input/netflix_titles.csv.gz'
@@ -10,5 +11,9 @@ FILE_NAME='s3a://mituca-repo1-raw-data/raw_input/netflix_titles.csv.gz'
 if __name__ == '__main__':
     credentials = AWSUtils(aws_profile).get_aws_credentials()
     spark = CreateSparkSession(credentials).get_spark_session()
-    df = spark.read.csv(FILE_NAME, inferSchema=True)
+    df = spark.read.options(header=True, inferSchema=True).csv(FILE_NAME)
+    
+    # Drop columns that have only null vallues
+    columns_to_drop = [col for col in df.columns if col.startswith('_c')]
+    df = df.drop(*columns_to_drop)
     df.show(10)
