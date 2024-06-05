c262088780985ba9d77dfaa9d46833d203dd398d
Mihai M <mihaimituca@gmail.com>
2024-06-05 09:51:14 +0300

    Added test

--- src/pyspark/dataframe_processing/netflix_titles.py
+++ src/pyspark/dataframe_processing/netflix_titles.py
@@ -1,7 +1,7 @@
 class TitlesDF:
     def __init__(self, spark_session, file_name):
-        raw_df = spark_session.read.options(header=True, inferSchema=True).csv(file_name)
-        self.__titles_df = self.drop_unwanted_columns(raw_df)
+        self.__titles_df = spark_session.read.options(header=True, inferSchema=True).csv(file_name)
+        self.sanitize_input()
 
 
     def get_df(self):
@@ -11,8 +11,8 @@ class TitlesDF:
         self.__titles_df = df
 
     def sanitize_input(self):
-        self.set_df(self.drop_unwanted_columns(self.get_df()))
+        self.set_df(self.__drop_unwanted_columns(self.get_df()))
 
-    def drop_unwanted_columns(self, df):
+    def __drop_unwanted_columns(self, df):
         columns_to_drop = [col for col in df.columns if col.startswith('_c')]
         return df.drop(*columns_to_drop)
--- tests/test_pyspark/test_dataframe_processing/test_netflix_titles.py
+++ tests/test_pyspark/test_dataframe_processing/test_netflix_titles.py
@@ -1,2 +1,31 @@
+import os
+from unittest.mock import patch
+import pandas as pd
+from src.pyspark.dataframe_processing.netflix_titles import TitlesDF
+
 class TestTitlesDF:
-    pass
+
+    @patch.object(TitlesDF, 'sanitize_input')
+    def test_init(self, mock_sanitize, spark_fixture):
+        # Create a temporary CSV file
+        df = pd.DataFrame({
+            'title': ['Title1', 'Title2'],
+            'year': [2001, 2002]
+        })
+        temp_csv_file = 'test.csv'
+        df.to_csv(temp_csv_file, index=False)
+
+    # Create an instance of MyClass with the Spark fixture and temporary CSV file
+        instance = TitlesDF(spark_fixture, temp_csv_file)
+    
+    # Verify that the CSV file is read with the correct options
+        actual_df = instance.get_df()
+        expected_df = spark_fixture.read.options(header=True, inferSchema=True).csv(temp_csv_file)
+    
+        assert actual_df.collect() == expected_df.collect()
+    
+    # Verify that sanitize_input is called once
+        mock_sanitize.assert_called_once()
+
+    # Clean up the temporary CSV file
+        os.remove(temp_csv_file)
