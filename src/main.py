f5fb3f3d3974aa60fb1fee4938b19242618cdd8d
Mihai M <mihaimituca@gmail.com>
2024-06-10 22:52:28 +0300

    Added changes

--- src/main.py
+++ src/main.py
@@ -1,20 +1,24 @@
 import os
+from dotenv import load_dotenv
 from utils.aws_utils import AWSUtils
 from utils.spark_session import CreateSparkSession
 from dataframe_processing.netflix_titles import Titles
 from dataframe_processing.users import Users
 from dataframe_processing.ratings import Ratings
 
+load_dotenv()
 aws_profile = os.environ['AWS_PROFILE']
 dataset_file_name = os.environ['TITLES_FILE_PATH']
 dataframe_destination = os.environ['DATAFRAME_DESTINATION']
 dataframe_writing_mode = os.environ['OVERWRITE_FILE_MODE']
+env = os.environ['ENVIRONMENT']
+s3_endpoint = os.environ['S3_ENDPOINT']
 
 
 if __name__ == '__main__':
     aws_utils = AWSUtils(aws_profile)
     credentials = aws_utils.get_aws_credentials()
-    spark = CreateSparkSession(credentials).get_spark_session()
+    spark = CreateSparkSession(credentials, env, s3_endpoint).get_spark_session()
 
     titles = Titles(spark, dataset_file_name)
     title_number = titles.titles_df.count()
@@ -27,9 +31,9 @@ if __name__ == '__main__':
     users.users_df.show(10)
     ratings.ratings_df.show(10)
 
-    # titles.titles_df.write.parquet(dataframe_destination + "titles/", mode=dataframe_writing_mode)
-    # users.users_df.write.parquet(dataframe_destination + "users/", mode=dataframe_writing_mode)
-    # ratings.ratings_df.write.parquet(dataframe_destination + "ratings/", mode=dataframe_writing_mode)
+    titles.titles_df.write.parquet(dataframe_destination + "titles/", mode=dataframe_writing_mode)
+    users.users_df.write.parquet(dataframe_destination + "users/", mode=dataframe_writing_mode)
+    ratings.ratings_df.write.parquet(dataframe_destination + "ratings/", mode=dataframe_writing_mode)
 
     # print(f"Max rating_id is: {ratings.get_df().agg({"id": "max"}).collect()[0][0]}")
     # print(f"Min rating_id is: {ratings.get_df().agg({"id": "min"}).collect()[0][0]}")
--- src/utils/spark_session.py
+++ src/utils/spark_session.py
@@ -4,12 +4,14 @@ from pyspark.sql import SparkSession
 
 
 class CreateSparkSession:
-    def __init__(self, aws_credentials):
-        self._spark_session = self._get_spark_session(aws_credentials)
+    def __init__(self, aws_credentials, env, s3_endpoint):
+        self._spark_session = self._get_spark_session(aws_credentials, env, s3_endpoint)
 
     @staticmethod
-    def _get_spark_session(aws_creds: tuple) -> pyspark.sql.session.SparkSession:
+    def _get_spark_session(aws_creds: tuple, env: str, s3_endpoint: str) -> pyspark.sql.session.SparkSession:
         conf = SparkConf()
+        if env == 'DEV':
+            conf.set("fs.s3a.endpoint", s3_endpoint)
         conf.set('fs.s3a.access.key', aws_creds[0])
         conf.set('fs.s3a.secret.key', aws_creds[1])
         return SparkSession.builder.config(conf=conf).getOrCreate()
