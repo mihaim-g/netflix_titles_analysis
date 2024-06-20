ae3198b65a7eab2b11077b31a59bef9e42e625ed
Mihai M <mihaimituca@gmail.com>
2024-06-20 16:55:34 +0300

    Small change to bucket name

--- terraform_files/variables.tf
+++ terraform_files/variables.tf
@@ -1,6 +1,6 @@
 variable "raw_data_bucket_name" {
   description = "The name of the S3 bucket"
-  default     = "mituca-repo1-raw-data-bucket"
+  default     = "mituca-repo1-raw-data"
 }
 
 variable "fetch_dataset_to_s3_name" {
