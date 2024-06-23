2202a2ef830c7f77edd7210365cb2aa033adbdbd
Mihai M <mihaimituca@gmail.com>
2024-06-23 14:00:05 +0300

    Changed spark creating class to singleton

--- src/utils/spark_session.py
+++ src/utils/spark_session.py
@@ -2,10 +2,20 @@ from pyspark import SparkConf
 from pyspark.sql import SparkSession
 
 
-class CreateSparkSession:
-    def __init__(self, aws_credentials: tuple, env: str, s3_endpoint: str) -> None:
-        self._spark_session = self._get_spark_session(aws_credentials, env, s3_endpoint)
+class CreateSparkSession(object):
+    _instance = None
 
+    def __new__(cls, aws_credentials: tuple, env: str, s3_endpoint: str) -> SparkSession:
+        if cls._instance is None:
+            cls._instance = super(CreateSparkSession, cls).__new__(cls)
+            cls._instance = cls._get_spark_session(aws_credentials, env, s3_endpoint)
+            # Put any initialization here.
+        return cls._instance
+
+# class CreateSparkSession:
+#     def __init__(self, aws_credentials: tuple, env: str, s3_endpoint: str) -> None:
+#         self._spark_session = self._get_spark_session(aws_credentials, env, s3_endpoint)
+#
     @staticmethod
     def _get_spark_session(aws_creds: tuple, env: str, s3_endpoint: str) -> SparkSession:
         conf = SparkConf()
@@ -15,6 +25,3 @@ class CreateSparkSession:
         conf.set('fs.s3a.access.key', aws_creds[0])
         conf.set('fs.s3a.secret.key', aws_creds[1])
         return SparkSession.builder.config(conf=conf).getOrCreate()
-
-    def get_spark_session(self) -> SparkSession:
-        return self._spark_session
