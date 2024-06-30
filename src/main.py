4f10938716a4e12b771adec9989e1b5f954aeb6a
Mihai M <mihaimituca@gmail.com>
2024-06-30 22:46:26 +0300

    Finished preparing dataset for reccomendations

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from utils.spark_session import CreateSparkSession
 from dataframe_processing.titles import Titles
 from dataframe_processing.users import Users
 from dataframe_processing.ratings import Ratings
-from reccomendations.recommend_titles import RecommendTitles
+from reccomendations.prepare_data import PrepareData
 
 
 if __name__ == '__main__':
@@ -18,14 +18,14 @@ if __name__ == '__main__':
         user_number = title_number * 5
         users = Users(spark, user_number)
         ratings = Ratings(spark, user_number, title_number)
+
         # Saving dataframes to S3 for use in future enhancements
         # titles.save_parquet_to_s3(env.dataframe_destination, env.dataframe_writing_mode)
         # users.save_parquet_to_s3(env.dataframe_destination, env.dataframe_writing_mode)
         # ratings.save_parquet_to_s3(env.dataframe_destination, env.dataframe_writing_mode)
-
-        ratings.ratings_df.show(10)
-        recommendations = RecommendTitles(ratings.ratings_df)
-        recommendations.recommendations_df.show(10)
+        
+        prepared_data = PrepareData(ratings.ratings_df)
+        prepared_data.prepared_data_df.show(10)
 
         # print(f"Max rating_id is: {ratings.get_df().agg({"id": "max"}).collect()[0][0]}")
         # print(f"Min rating_id is: {ratings.get_df().agg({"id": "min"}).collect()[0][0]}")
